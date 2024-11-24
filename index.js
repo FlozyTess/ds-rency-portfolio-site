ec69b99e2af4d20469668818301f3fc48e54de5e
FlozyTess <cecilrency23@gmail.com>
2024-11-24 13:49:00 +0300

    created and styled the 2nd element on js file

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ h2.style.color = "#eeeeee";
 h2.style.fontSize = "36px";
 h2.style.marginTop = "40px";
 h2.style.marginLeft = "20px";
+const p = document.createElement("p");
+p.textContent = "A passionate data analyst and software developer based in Nairobi,Kenya";
+document.querySelector("body").appendChild(p)
+p.style.color = "#eeeeee";
+p.style.fontSize = "25px";
+p.style.marginTop = "5px";
+p.style.marginLeft = "15px";
