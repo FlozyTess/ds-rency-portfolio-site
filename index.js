f60c6307eac7dda04090869e45ce38a01b5882f0
FlozyTess <cecilrency23@gmail.com>
2024-11-24 13:24:19 +0300

    styled the js file

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const h2 = document.createElement("h2");
 h2.textContent = "Florence Simba";
 document.querySelector("body").appendChild(h2);
 h2.style.color = "#eeeeee";
+h2.style.fontSize = "36px";
+h2.style.marginTop = "40px";
+h2.style.marginLeft = "20px";
