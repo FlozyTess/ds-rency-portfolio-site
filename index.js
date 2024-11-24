ca24e91bb2936c80d2d46f4ef08cbbc7ad9dffb5
FlozyTess <cecilrency23@gmail.com>
2024-11-24 15:03:49 +0300

    created and styled in js and css files.

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ h2.style.fontSize = "36px";
 h2.style.marginTop = "40px";
 h2.style.marginLeft = "20px";
 const p = document.createElement("p");
-p.textContent = "A passionate data analyst and software developer based in Nairobi,Kenya";
+p.textContent = "A passionate data analyst and software developer based in Nairobi,Kenya.";
 document.querySelector("body").appendChild(p)
 p.style.color = "#eeeeee";
 p.style.fontSize = "25px";
 p.style.marginTop = "5px";
 p.style.marginLeft = "15px";
+const p1 = document.createElement("p1")
+p1.textContent = "Welcome to my space,where I share my passion and expertise in big data analytics and software development.Explore my projects and see how I solve complex problems with innovative solutions.";
+document.querySelector("body").appendChild(p1);
+p1.style.color = "#eeeeee";
+p1.style.fontSize = "22px";
+p1.style.marginTop = "5px";
+p1.style.marginLeft = "15px";
