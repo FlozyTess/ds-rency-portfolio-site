c9a18383517da8fe694d8d414429a34a76f55bb9
FlozyTess <cecilrency23@gmail.com>
2024-11-25 08:42:57 +0300

    positioned the js file

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const h2 = document.createElement("h2");
 h2.textContent = "Florence Simba";
 h2.style.color = "#ffffff";
 h2.style.fontSize = "40px";
-h2.style.marginTop = "20px";
-h2.style.textAlign = "center";
+h2.style.margin = "0";
+h2.style.textAlign = "left";
 
 const p = document.createElement("p");
 p.textContent = "A passionate data analyst and software developer based in Nairobi, Kenya.";
 p.style.color = "#ffffff";
 p.style.fontSize = "30px";
-p.style.marginTop = "5px";
-p.style.textAlign = "center";
+p.style.marginTop = "2px";
+p.style.textAlign = "left";
 
 const nav = document.querySelector("header nav"); 
 nav.insertAdjacentElement("afterend", h2);
